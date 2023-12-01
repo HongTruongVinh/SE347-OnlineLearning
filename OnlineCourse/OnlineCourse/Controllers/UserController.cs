cc423df85a8f5c0192d031e526a7f1df7a6b4cd2
TendouHakai <thanhconghoahai@gmail.com>
2023-12-01 23:10:08 +0700

    code controller and create view

---
+++ OnlineCourse/OnlineCourse/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using Model.Dao;
+using Model.Models;
+using OnlineCourse.Common;
+using OnlineCourse.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Razor.Editor;
+using System.Web.Routing;
+using System.Web.Security;
+
+namespace OnlineCourse.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [System.Web.Mvc.HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            return View("model");
+        }
+
+        public ActionResult LogUot()
+        {
+            return new RedirectToRouteResult(new RouteValueDictionary(new { controller = "Home", action = "Index" }));
+        }
+
+
+        public ActionResult ProfileUser()
+        {
+            return View();
+        }
+
+        UserLogin SetUserSession(User user)
+        {
+            var usersession = new UserLogin();
+            return usersession;
+        }
+    }
+}
